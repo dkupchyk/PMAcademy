d3faee7cd3791ef13b21d04ba8061933347ca8b2
Daryna Kupchyk <dasha.kupchik28@gmail.com>
2021-01-02 17:38:21 +0200

    Implemented the task 2

--- lab5/js/requests.js
+++ lab5/js/requests.js
@@ -1,19 +1,48 @@
-// Задание 1
-// Запросить у пользователя два числа, n и m - вывести все числа в диапазоне между ними.
+const task1 = function () {
+    if (task1N.value.length === 0 || task1M.value.length === 0) {
+        alert("ЗАДАНИЕ 1: Введите значения n и m.")
+    } else {
+        let min = Math.min(+task1N.value, +task1M.value)
+        let max = Math.max(+task1N.value, +task1M.value)
 
-document.getElementById("task-1-button").addEventListener("click", task1);
-
-let task1N = document.getElementById("task-1-n");
-let task1M = document.getElementById("task-1-m");
-
-function task1() {
-    let min = Math.min(+task1N.value, +task1M.value)
-    let max = Math.max(+task1N.value, +task1M.value)
-
-    let rangeArray = range(min, max)
-    document.getElementById("task-1-answer").innerText = "Ответ: " + rangeArray.join(", ");
+        let rangeArray = "-";
+        if (min !== max) {
+            rangeArray = range(min, max).join(", ");
+        }
+        document.getElementById("task-1-answer").innerText = "Ответ: " + rangeArray;
+    }
 }
 
 function range(start, end) {
     return Array(end - start - 1).fill().map((_, idx) => start + idx + 1)
 }
+
+const task2 = function () {
+    if (task2N.value.length === 0) {
+        alert("ЗАДАНИЕ 2: Введите значения n.")
+    } else if (task2M.value.length === 0){
+        document.getElementById("task-2-answer").innerText = "Ответ: " + task2Arrow(+task2N.value);
+    } else {
+        //вызов функции с стрелочным синтаксисом и аналог с function expression
+        // document.getElementById("task-2-answer").innerText = "Ответ: " + task2Arrow(+task2N.value, +task2M.value);
+
+        // аналог с function expression
+        document.getElementById("task-2-answer").innerText = "Ответ: " + task2FuncExp(+task2N.value, +task2M.value);
+    }
+}
+
+const task2FuncExp = function (x, y=2) {
+   return x ** y
+}
+const task2Arrow = (x, y=2) => x ** y;
+
+
+
+let task1N = document.getElementById("task-1-n");
+let task1M = document.getElementById("task-1-m");
+
+let task2N = document.getElementById("task-2-n");
+let task2M = document.getElementById("task-2-m");
+
+document.getElementById("task-1-button").addEventListener("click", task1);
+document.getElementById("task-2-button").addEventListener("click", task2);
