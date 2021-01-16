b7f1a284e6ee3d2d4d88bf902f4cd5082170126d
Daryna Kupchyk <dasha.kupchik28@gmail.com>
2021-01-16 12:15:02 +0200

    Implemented main functionality

--- lab7/js/main.js
+++ lab7/js/main.js
@@ -1,61 +1,77 @@
-// Initialization of all files
 const containerElement = document.querySelector(`.container`);
-const allFiles = containerElement.querySelectorAll(`.file`);
 
-let openedPopOver = null;
-
-// Popovers
 const popoverHandler = (event) => {
     let state = event.target.className;
 
-    if (state === 'file') {
-        let allPopovers = document.querySelectorAll('.popover')
+    deleteOpenedPopovers();
 
-        if (allPopovers.length) {
-            allPopovers[0].remove();
-        }
+    if (!state) {
+        let popover = document.createElement('div');
+        popover.classList.add('popover');
 
-        openedPopOver = document.createElement('div');
-        openedPopOver.classList.add('popover');
-        openedPopOver.innerHTML = "<p class='edit' onclick='editFile(event)'>Edit</p>" +
-            "<p class='delete' onclick='deleteFile(event)'>Delete</p>"
-        event.target.append(openedPopOver);
+        popover.style.left = event.pageX + 'px';
+        popover.style.top = event.pageY + 'px';
+
+        popover.innerHTML = "<p class='create' onclick='createFile()'>Create</p>";
+        event.target.append(popover);
+
+    } else if (state === 'file') {
+        let popover = document.createElement('div');
+        popover.classList.add('popover');
+        popover.innerHTML = "<p class='edit' onclick='editFile(event)'>Edit</p>" +
+            "<p class='delete' onclick='deleteFile(event)'>Delete</p>";
+        event.target.append(popover);
     }
 };
 
 const editFile = (event) => {
     let mainDiv = event.target.parentElement.parentElement;
     let fileName = mainDiv.firstChild.nodeValue;
-    let refactoredFileName = fileName.replace(/\s+/g, ' ');
-    let newName = prompt('Enter file name', refactoredFileName);
-
-    while (!checkIfValidFileName(newName)) {
-        newName = prompt('ERROR! Enter a valid file name', refactoredFileName);
-    }
-
-    mainDiv.innerText = newName;
+    let oldName = fileName.replace(/\s+/g, ' ');
+    let newName = isValidName(oldName, 'Enter file name', 'ERROR! Enter a valid file name');
+    return newName ? mainDiv.innerText = newName : mainDiv.innerText = oldName;
 }
 
 const deleteFile = (event) => {
     event.target.parentElement.parentElement.remove();
 }
 
+const createFile = () => {
+    let name = isValidName('', 'Name', 'ERROR! Enter a valid file name');
+
+    if (name === null) {
+        return;
+    }
+    
+    const newFile = document.createElement('div');
+    newFile.innerHTML = name;
+    newFile.classList.add('file');
+    containerElement.appendChild(newFile);
+}
+
 const checkIfValidFileName = (name) => {
     return /^(.|\s)*\S(.|\s)*$|^[^\\\/\:\*\?\"\<\>\|\.]+(\.[^\\\/\:\*\?\"\<\>\|\.]+)+$/.test(name);
 }
 
-// const createNewFile = () => {
-//     let name = prompt("Name:");
-//     const newFile = document.createElement('div');
-//     newFile.innerHTML = name;
-//     newFile.classList.add('file');
-//     containerElement.appendChild(newFile);
-// }
+const deleteOpenedPopovers = () => {
+    let allPopovers = document.querySelectorAll('.popover')
 
-// document.addEventListener('click', createNewFilePopover);
-
-for (const file of allFiles) {
-    file.draggable = true;
-    file.addEventListener('click', popoverHandler);
+    if (allPopovers.length) {
+        allPopovers[0].remove();
+    }
 }
+
+const isValidName = (oldName, mainPrompt, errorPrompt) => {
+    let newName = prompt(mainPrompt, oldName);
+
+    while (!checkIfValidFileName(newName)) {
+        if (newName === null) return null;
+        newName = prompt(errorPrompt, oldName);
+    }
+
+    return newName;
+}
+
+document.addEventListener('click', popoverHandler);
+
 
