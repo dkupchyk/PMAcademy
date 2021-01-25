8702c2478cc17cbb880debd344a9ef260261b075
Daryna Kupchyk <dasha.kupchik28@gmail.com>
2021-01-25 14:16:16 +0200

    Implemented 1 and 2 tasks

--- lab6/js/index.js
+++ lab6/js/index.js
@@ -1,101 +1,92 @@
-
-// FROM
-// {
-//     id: 1,
-//     name: "Leanne Graham",
-//     username: "Bret",
-//     email: "Sincere@april.biz",
-//     registrationDate: "Tue, 12 Jan 2021 12: 28: 18 + 0000",
-//     address: {
-//         street: "Kulas Light",
-//         suite: "Apt. 556",
-//         city: "Gwenborough",
-//         zipcode: "92998-3874",
-//         geo:
-//         {
-//             lat: "-37.3159",
-//                 lng: "81.1496"
-//         }
-//     },
-//     phone: "050 866-66-66",
-//     website: "hildegard.org",
-//     company:{
-//         name: "Romaguera-Crona",
-//         catchPhrase: "Multi-layered client-server neural-net",
-//         bs: "harness real-time e-markets"
-//     }
-// }
-
-// TO
-
-// {
-//     1: {
-//         id: 1,
-//         name: "Leanne Graham",
-//         username: "Bret",
-//         email: "Sincere@april.biz",
-//         registrationDate: "Tue, 12 Jan 2021 12: 28: 18 + 0000",
-//         address:{
-//             street: "Kulas Light",
-//             suite: "Apt. 556",
-//             city: "Gwenborough",
-//             zipcode: "92998-3874",
-//             geo:{
-//                 lat: "-37.3159",
-//                 lng: "81.1496"
-//             }
-//         },
-//         phone: "050 866-66-66",
-//         website: "hildegard.org",
-//         company:{
-//             name: "Romaguera-Crona",
-//             catchPhrase: "Multi-layered client-server neural-net",
-//             bs: "harness real-time e-markets"
-//         }
-//     }
-// }
-//
-
 let usersHashMap = {};
-let currentId = 1;
 
 USERS.forEach(user => {
-    usersHashMap[currentId] =  {
+    usersHashMap[user.id] = {
         id: user.id,
         name: user.name,
         username: user.username,
         email: user.email,
         registrationDate: user.registrationDate,
-        address:{
+        address: {
             street: user.address.street,
             suite: user.address.suite,
             city: user.address.city,
             zipcode: user.address.zipcode,
-            geo:{
+            geo: {
                 lat: user.address.geo.lat,
                 lng: user.address.geo.lng
             }
         },
         phone: user.phone,
         website: user.website,
-        company:{
+        company: {
             name: user.company.name,
             catchPhrase: user.company.catchPhrase,
             bs: user.company.bs
         }
     };
-    currentId++;
 })
 
 
-for (let x in usersHashMap)
-{
+for (let x in usersHashMap) {
     console.log('Key:\n—- ' + x + '\n');
     console.log('Values: ');
     let value = usersHashMap[x];
-    for (let y in value)
-    {
+    for (let y in value) {
         console.log('—- ' + y + ':' + value[y]);
     }
     console.log('\n');
 }
+
+const printUser = function (obj, spaces = 1) {
+    let string = '';
+
+    let space = `\xa0`.repeat(4 * spaces);
+
+    for (let prop in obj) {
+        string += space;
+
+        if (typeof obj[prop] === 'object') {
+            string += `<span class="property">${prop}</span>: { </br> ${printUser(obj[prop], spaces + 1)}`;
+            string += `${space}} </br>`;
+        } else {
+            string += `<span class="property">${prop}</span> : ${obj[prop]}; </br>`;
+        }
+    }
+
+    return string;
+}
+
+
+// TASK 1
+// Запросить у пользователя id - вывести данные по пользователю.
+
+let task1Id = document.getElementById("task-1-id");
+let task2Id = document.getElementById("task-2-id");
+
+document.getElementById("task-1-button").addEventListener("click", () => {
+    if (valid("ЗАДАНИЕ 1: Введите id пользователя.", 0, task1Id.value.length)) {
+        let user = usersHashMap[task1Id.value];
+        console.log(printUser(user))
+
+        let answer = `Ответ: `;
+        (usersHashMap.hasOwnProperty(task1Id.value))
+            ? answer += `<br> { <br> ${printUser(user)} }`
+            : answer += `такого пользователя не существует.` ;
+
+        document.getElementById("task-1-answer").innerHTML = answer;
+    }
+});
+
+document.getElementById("task-2-button").addEventListener("click", () => {
+    if (valid("ЗАДАНИЕ 2: Введите id пользователя.", 0, task2Id.value.length)) {
+        let user = usersHashMap[task2Id.value];
+
+        let answer = `Ответ: `;
+        (usersHashMap.hasOwnProperty(task2Id.value))
+            ? answer += `<br> { <br> ${printUser(user.address)} }`
+            : answer += `такого пользователя не существует.` ;
+
+        document.getElementById("task-2-answer").innerHTML = answer;
+    }
+});
---
+++ lab6/js/validation.js
@@ -0,0 +1,9 @@
+const valid = function (alertText, equalExpr, ...values) {
+    for (let value of values) {
+        if (value === equalExpr) {
+            alert(alertText);
+            return false;
+        }
+    }
+    return true;
+}
