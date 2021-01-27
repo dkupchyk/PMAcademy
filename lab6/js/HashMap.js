481042e32a5fef49213390c6e5ab89bd4a839f81
Daryna Kupchyk <dasha.kupchik28@gmail.com>
2021-01-27 18:47:44 +0200

    Implemented task 5

---
+++ lab6/js/HashMap.js
@@ -0,0 +1,94 @@
+class HashMap {
+
+    constructor(array) {
+        this.initialize(array);
+    }
+
+    initialize(array) {
+        array.forEach(user => {
+            this[user.id] = {
+                id: user.id,
+                name: user.name,
+                username: user.username,
+                email: user.email,
+                registrationDate: user.registrationDate,
+                address: {
+                    street: user.address.street,
+                    suite: user.address.suite,
+                    city: user.address.city,
+                    zipcode: user.address.zipcode,
+                    geo: {
+                        lat: user.address.geo.lat,
+                        lng: user.address.geo.lng
+                    }
+                },
+                phone: user.phone,
+                website: user.website,
+                company: {
+                    name: user.company.name,
+                    catchPhrase: user.company.catchPhrase,
+                    bs: user.company.bs
+                }
+            };
+        });
+    }
+
+    displayAllUsers() {
+        for (let x in this) {
+            console.log('Key:\n—- ' + x + '\n');
+            console.log('Values: ');
+            let value = this[x];
+            for (let y in value) {
+                console.log('—- ' + y + ':' + value[y]);
+            }
+            console.log('\n');
+        }
+    }
+
+    printUser(obj, spaces = 1) {
+        let string = '';
+        let space = `\xa0`.repeat(4 * spaces);
+
+        for (let prop in obj) {
+            string += space;
+
+            if (typeof obj[prop] === 'object') {
+                string += `<span class="property">${prop}</span>: { </br> ${this.printUser(obj[prop], spaces + 1)}`;
+                string += `${space}} </br>`;
+            } else {
+                string += `<span class="property">${prop}</span> : ${obj[prop]}; </br>`;
+            }
+        }
+
+        return string;
+    }
+
+    filterByCompanyName(companyName) {
+        let result = {}
+        for (let i in this) {
+            let user = this[i];
+            if (user.company.name === companyName) {
+                result[user.id] = user;
+            }
+        }
+        return result;
+    }
+
+    getUserById(userId) {
+        for (let i in this) {
+            let user = this[i];
+            if (user.id === userId) {
+                return user;
+            }
+        }
+
+        return -1;
+    }
+
+    setPhone(userId, newPhone) {
+        let user = this.getUserById(userId);
+        if (user !== -1) {
+            user.phone = newPhone;
+        }
+    }
+}
--- lab6/js/index.js
+++ lab6/js/index.js
@@ -1,78 +1,19 @@
-let usersHashMap = {};
-
-USERS.forEach(user => {
-    usersHashMap[user.id] = {
-        id: user.id,
-        name: user.name,
-        username: user.username,
-        email: user.email,
-        registrationDate: user.registrationDate,
-        address: {
-            street: user.address.street,
-            suite: user.address.suite,
-            city: user.address.city,
-            zipcode: user.address.zipcode,
-            geo: {
-                lat: user.address.geo.lat,
-                lng: user.address.geo.lng
-            }
-        },
-        phone: user.phone,
-        website: user.website,
-        company: {
-            name: user.company.name,
-            catchPhrase: user.company.catchPhrase,
-            bs: user.company.bs
-        }
-    };
-})
-
-
-for (let x in usersHashMap) {
-    console.log('Key:\n—- ' + x + '\n');
-    console.log('Values: ');
-    let value = usersHashMap[x];
-    for (let y in value) {
-        console.log('—- ' + y + ':' + value[y]);
-    }
-    console.log('\n');
-}
-
-const printUser = function (obj, spaces = 1) {
-    let string = '';
-
-    let space = `\xa0`.repeat(4 * spaces);
-
-    for (let prop in obj) {
-        string += space;
-
-        if (typeof obj[prop] === 'object') {
-            string += `<span class="property">${prop}</span>: { </br> ${printUser(obj[prop], spaces + 1)}`;
-            string += `${space}} </br>`;
-        } else {
-            string += `<span class="property">${prop}</span> : ${obj[prop]}; </br>`;
-        }
-    }
-
-    return string;
-}
-
-
-// TASK 1
-// Запросить у пользователя id - вывести данные по пользователю.
+let usersHashMap = new HashMap(USERS);
+usersHashMap.displayAllUsers();
 
 let task1Id = document.getElementById("task-1-id");
 let task2Id = document.getElementById("task-2-id");
 let task3Company = document.getElementById("task-3-company");
+let task5Id = document.getElementById("task-5-id");
+let task5Phone = document.getElementById("task-5-phone");
 
 document.getElementById("task-1-button").addEventListener("click", () => {
     if (valid("ЗАДАНИЕ 1: Введите id пользователя.", 0, task1Id.value.length)) {
-        let user = usersHashMap[task1Id.value];
-        console.log(printUser(user))
+        let user = usersHashMap.getUserById(+task1Id.value);
 
         let answer = `Ответ: `;
         (usersHashMap.hasOwnProperty(task1Id.value))
-            ? answer += `<br> { <br> ${printUser(user)} }`
+            ? answer += `<br> { <br> ${usersHashMap.printUser(user)} }`
             : answer += `такого пользователя не существует.`;
 
         document.getElementById("task-1-answer").innerHTML = answer;
@@ -81,11 +22,11 @@ document.getElementById("task-1-button").addEventListener("click", () => {
 
 document.getElementById("task-2-button").addEventListener("click", () => {
     if (valid("ЗАДАНИЕ 2: Введите id пользователя.", 0, task2Id.value.length)) {
-        let user = usersHashMap[task2Id.value];
+        let user = usersHashMap.getUserById(+task2Id.value);
 
         let answer = `Ответ: `;
         (usersHashMap.hasOwnProperty(task2Id.value))
-            ? answer += `<br> { <br> ${printUser(user.address)} }`
+            ? answer += `<br> { <br> ${usersHashMap.printUser(user.address)} }`
             : answer += `такого пользователя не существует.`;
 
         document.getElementById("task-2-answer").innerHTML = answer;
@@ -94,15 +35,15 @@ document.getElementById("task-2-button").addEventListener("click", () => {
 
 document.getElementById("task-3-button").addEventListener("click", () => {
     if (valid("ЗАДАНИЕ 3: Введите компанию.", 0, task3Company.value.length)) {
-        const filtered = filter(usersHashMap, task3Company.value);
+        const filtered = usersHashMap.filterByCompanyName(task3Company.value);
 
         let answer = `Ответ: `;
         if (Object.keys(filtered).length === 0) {
             answer += `такого пользователя не существует.`;
         } else {
             for (let user in filtered) {
-                answer += `<br> { <br> ${printUser(filtered[user])} } <br>`
-                answer += '_'.repeat(80) + `<br>`;
+                answer += `<br> { <br> ${usersHashMap.printUser(filtered[user])} } <br>`
+                answer += `<p class="centered">==========</p><br>`;
             }
         }
 
@@ -119,14 +60,19 @@ document.getElementById("task-4-button").addEventListener("click", () => {
     }
 });
 
-const filter = (hashMap, companyName) => {
-    let result = {}
-    for (let i in hashMap) {
-        let user = hashMap[i];
-        if (user.company.name === companyName) {
-            result[user.id] = user;
-        }
-    }
-    return result;
-}
+document.getElementById("task-5-button").addEventListener("click", () => {
+    if (valid("ЗАДАНИЕ 5: Введите правильные значения.", 0, task5Id.value.length, task5Phone.value.length)) {
 
+        const isValidPhone = /^\s*\(\d{3}\) \d{3}-\d{2}-\d{2}\s*$/.test(task5Phone.value);
+        let answer = `Ответ: `;
+
+        if (!isValidPhone) {
+            answer += `введите валидный номер телефона.`;
+        } else {
+            usersHashMap.setPhone(+task5Id.value, task5Phone.value)
+            answer += `номер телефона успешно изменён.`;
+        }
+
+        document.getElementById("task-5-answer").innerHTML = answer;
+    }
+});
