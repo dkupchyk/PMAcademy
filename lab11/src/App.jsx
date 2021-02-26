ca74b14b92844f8a08978310fe94d0f2b6727490
Daryna Kupchyk <dasha.kupchik28@gmail.com>
2021-02-26 14:17:01 +0200

    Added lab 11

---
+++ lab11/src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const name = 'kjdshfd';
+  
+  return (
+    <div className="App">
+      <p>Hello, {name}</p>
+    </div>
+  );
+}
+
+export default App;
