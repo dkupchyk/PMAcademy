5243df875da97e0ffb51cd807c74b56cdcc434e7
Daryna Kupchyk <dasha.kupchik28@gmail.com>
2021-02-27 16:00:49 +0200

    Implemented main structure of the app

--- lab11/src/App.jsx
+++ lab11/src/App.jsx
@@ -1,14 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import {API} from "./constants";
 
 function App() {
-  const name = 'kjdshfd';
 
-  return (
-    <div className="App">
-      <p>Hello, {name}</p>
-    </div>
-  );
+    const [query, setQuery] = useState('');
+    const [weather, setWeather] = useState({});
+
+    const search = evt => {
+        if (evt.key === "Enter") {
+            fetch(`${API.url}weather?q=${query}&units=metric&APPID=${API.key}`)
+                .then(res => res.json())
+                .then(result => {
+                    setQuery('');
+                    setWeather(result);
+                    console.log(result)
+                });
+        }
+
+        console.log(weather)
+        console.log(Object.keys(weather).length === 0)
+    }
+
+    return (
+        <div className="App">
+
+            <div className="search-box">
+                <input type="text"
+                       className="search-bar"
+                       placeholder="Enter city.."
+                       onChange={e => setQuery(e.target.value)}
+                       value={query}
+                       onKeyPress={search}/>
+            </div>
+
+            <div>
+                <div className="location-box">
+                    <div
+                        className="location">{Object.keys(weather).length === 0 ? '' : weather.name + ', ' + weather.sys.country}</div>
+                    <div className="date">date</div>
+                </div>
+
+                <div className="weather-box">
+                    <div
+                        className="temp">{Object.keys(weather).length === 0 ? '' : Math.round(weather.main.temp) + '°c'}</div>
+                    <div className="weather">{Object.keys(weather).length === 0 ? '' : weather.weather[0].main}</div>
+                </div>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
---
+++ lab11/src/constants.js
@@ -0,0 +1,4 @@
+export const API = {
+    key: 'd2e604dc8a07b6adfa7d77cbf848f3cb\n',
+    url: 'https://api.openweathermap.org/data/2.5/'
+}
