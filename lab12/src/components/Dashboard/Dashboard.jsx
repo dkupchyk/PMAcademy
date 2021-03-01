3d35f3c1ad9a9f72621f5e0f38cfccadc5c27462
Daryna Kupchyk <dasha.kupchik28@gmail.com>
2021-03-01 23:50:23 +0200

    Added basic stylying

--- lab12/src/components/Dashboard/Dashboard.jsx
+++ lab12/src/components/Dashboard/Dashboard.jsx
@@ -7,9 +7,9 @@ import Table from "../Table/Table";
 class Dashboard extends React.Component {
     render() {
         return <div className="dashboard-container">
-            <Table name="1dsmvknlkd"/>
-            <Table name="1dsmvknlkd"/>
-            <Table name="1dsmvknlkd"/>
+            <Table name="Good things"/>
+            <Table name="Bad things"/>
+            <Table name="Action items"/>
         </div>;
     }
 }
